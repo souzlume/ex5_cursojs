5a3ab2cd248e974a7f16b7ffca39ba0d12a4ad5a
souzlume <lucassmelo2020@gmail.com>
2025-07-14 17:04:58 -0300

    exercício sobre funções, html e outros

---
+++ aula5/ex5.js
@@ -0,0 +1,27 @@
+function exerc () {
+    const form = document.getElementsByClassName("form");
+    const resultado = document.getElementsByClassName("resultado");
+    const pessoas = [];
+
+    form.addEventListener('submit', recebeEventoForm);
+
+    function recebeEventoForm (evento) {
+        evento.preventDefault();
+        const nome = form.getElementsByClassName("nome");
+        const sobrenome = form.getElementsByClassName("sobrenome");
+        const peso = form.getElementsByClassName("peso");
+        const altura = form.getElementsByClassName("altura");
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+
+        console.log(pessoas);
+
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`;
+    };
+}
+exerc();
